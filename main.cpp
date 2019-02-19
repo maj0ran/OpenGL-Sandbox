1a2c04d22cf734b9032bd44130ace3b49a7c4575
mrn <mn.cichy@gmail.com>
2019-02-19 21:36:40 +0100

    + inserted glm

--- main.cpp
+++ main.cpp
@@ -57,6 +57,15 @@ GLFWwindow* initWindow(const char* windowTitle) {
 int main() {
     char infoLog[512];
 
+    glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+    glm::mat4 trans = glm::mat4(1.0f);
+
+    trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+
+    vec = trans * vec;
+
+    std::cout << vec.x << vec.y << vec.z << std::endl;
+
     GLFWwindow* window = initWindow("OpenGL-Sandbox");
     glfwMakeContextCurrent(window);
 
